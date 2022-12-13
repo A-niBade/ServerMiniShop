fc0f95100582c0e7bb4b1fa2341c2e5ff8353786
Anibal <joan.maldonadoa@gmail.com>
2022-12-13 11:38:52 -0300

    Modelos de productos creados

---
+++ src/models/pinturaModel.js
@@ -0,0 +1,54 @@
+/*
+ * Creado por: Anibal
+ * Fecha creación: 9-12-2022
+ */
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const pinturaSchema = new Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  sku: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  linea: {
+    type: String,
+    required: true,
+  },
+  contenido: {
+    type: String,
+    required: true,
+  },
+  nombre: {
+    type: String,
+    required: true,
+  },
+  marca: {
+    type: String,
+    required: true,
+  },
+  uso: {
+    type: String,
+    required: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+  },
+  destacado: {
+    type: Boolean,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("pinturas", pinturaSchema);
